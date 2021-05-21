92f99fc01b22fdc985273a1d778c2238241b636c
cmunhozprog03 <cmunhozprog03@hotmail.com>
2021-05-21 14:12:52 -0300

    Lesson 23 completed

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\RoleResource;
 use App\Models\Role;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,18 +11,18 @@ class RoleController extends Controller
 {
     public function index()
     {
-        return Role::all();
+        return RoleResource::collection(Role::all());
     }
 
     public function store(Request $request)
     {
         $role = Role::create($request->only('name'));
-        return response($role, Response::HTTP_CREATED);
+        return response(new RoleResource($role), Response::HTTP_CREATED);
     }
 
     public function show($id)
     {
-        return Role::find($id);
+        return new RoleResource(Role::find($id));
     }
 
     public function update(Request $request, $id)
@@ -29,7 +30,7 @@ class RoleController extends Controller
         $role = Role::find($id);
         $role->update($request->only('name'));
 
-        return response($role, Response::HTTP_ACCEPTED);
+        return response(new RoleResource($role), Response::HTTP_ACCEPTED);
     }
 
     public function destroy($id)
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
     
     public function index()
     {
-        return UserResource::collection(User::with('role')->paginate());
+        return UserResource::collection(User::paginate());
     }
 
    
